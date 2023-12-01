10016399bee343cf1f05551b3ea2410046eac207
ionut <opreaionut035@gmail.com>
2023-12-01 22:00:19 +0200

    Added script file

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #! /bin/bash
 javac Main.java
-
+echo "Compiling..."
 if [ -z "Main.class" ];then
+    echo "Compiled"
+    if [ $(uname) == "Linux" ];then
+        if [ grep -q "sudokuGame" ~/.bashrc ];then
 
+            echo "alias sudokugame=$(java $(pwd)/Main)" >> ~/.bashrc
+
+
+        fi
+    fi
 
 
 
